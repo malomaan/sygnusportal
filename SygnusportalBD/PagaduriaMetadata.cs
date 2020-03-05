0b491159edf34e0173f1e754a19ee78feabd06d5
malomaan <malomaan@msn.com>
2020-03-05 08:50:10 -0500

    Se genero el controlador y las vistas de Pagadurías

---
+++ SygnusportalBD/PagaduriaMetadata.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace SygnusportalBD
+{
+    [MetadataType(typeof(Pagaduria.Metadata))]
+    public partial class Pagaduria
+    {
+        sealed class Metadata
+        {
+            [DisplayName("ID")]
+            [DataType(DataType.Text)]
+            [Required(ErrorMessage = "Debe ingresar el {0}")]
+            public int pag_id { get; set; }
+
+            [DisplayName("Codigo")]
+            [DataType(DataType.Text)]
+            [Required(ErrorMessage = "Debe ingresar el {0}")]
+            [StringLength(50)]
+            public string pag_codigo { get; set; }
+
+            [DisplayName("NIT")]
+            [DataType(DataType.Text)]
+            [Required(ErrorMessage = "Debe ingresar el {0}")]
+            public string pag_nit { get; set; }
+
+            [DisplayName("DV")]
+            [DataType(DataType.Text)]
+            [Required(ErrorMessage = "Debe ingresar el {0}")]
+            [StringLength(1)]
+            public Nullable<int> pag_dv { get; set; }
+
+            [DisplayName("Nombre")]
+            [DataType(DataType.Text)]
+            [Required(ErrorMessage = "Debe ingresar el {0}")]
+            [StringLength(250)]
+            public string pag_nombre { get; set; }
+
+            [DisplayName("Direccion")]
+            [DataType(DataType.Text)]
+            [Required(ErrorMessage = "Debe ingresar el {0}")]
+            [StringLength(250)]
+            public string pag_direccion { get; set; }
+
+            [DisplayName("Ciudad")]
+            [DataType(DataType.Text)]
+            [Required(ErrorMessage = "Debe ingresar el {0}")]
+            public string ciu_codigo { get; set; }
+
+            [DisplayName("Telefonos")]
+            [DataType(DataType.Text)]
+            [Required(ErrorMessage = "Debe ingresar el {0}")]
+            [StringLength(150)]
+            public string pag_telefonos { get; set; }
+
+            [DisplayName("URL")]
+            [DataType(DataType.Url)]
+            [Required(ErrorMessage = "Debe ingresar el {0}")]
+            public string pag_url { get; set; }
+
+            [DisplayName("Email")]
+            [DataType(DataType.EmailAddress)]
+            [Required(ErrorMessage = "Debe ingresar el {0}")]
+            public string pag_mail { get; set; }
+
+            [DisplayName("Logo")]
+            [DataType(DataType.Text)]
+            [Required(ErrorMessage = "Debe ingresar el {0}")]
+            public string pag_logo { get; set; }
+
+            [DisplayName("SygnusURL")]
+            [DataType(DataType.Url)]
+            [Required(ErrorMessage = "Debe ingresar el {0}")]
+            public string pag_urlsygnus { get; set; }
+
+            [DisplayName("Color")]
+            [DataType(DataType.Text)]
+            [Required(ErrorMessage = "Debe ingresar el {0}")]
+            [StringLength(250)]
+            public string pag_color { get; set; }
+
+            [DisplayName("Tipo")]
+            [DataType(DataType.Text)]
+            [Required(ErrorMessage = "Debe ingresar el {0}")]
+            public Nullable<int> tpa_id { get; set; }
+
+            [DisplayName("Empresa")]
+            [DataType(DataType.Text)]
+            [Required(ErrorMessage = "Debe ingresar el {0}")]
+            public string emp_nit { get; set; }
+
+            [DisplayName("IDEntidad")]
+            [DataType(DataType.Text)]
+            [Required(ErrorMessage = "Debe ingresar el {0}")]
+            public Nullable<int> ent_id { get; set; }
+
+            [DisplayName("Activa")]
+            [Required(ErrorMessage = "Debe ingresar el {0}")]
+            public Nullable<bool> pag_activa { get; set; }
+
+            [DisplayName("Fax")]
+            [DataType(DataType.Text)]
+            [Required(ErrorMessage = "Debe ingresar el {0}")]
+            [StringLength(250)]
+            public string pag_fax { get; set; }
+
+
+        }
+    }
+}
---
+++ SygnusportalWEB/Controllers/PagaduriasController.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SygnusportalBD;
+
+namespace SygnusportalWEB.Controllers
+{
+    public class PagaduriasController : Controller
+    {
+        private sygnusportalEntities db = new sygnusportalEntities();
+
+        // GET: Pagadurias
+        public ActionResult Index()
+        {
+            var pagaduria = db.Pagaduria.Include(p => p.Ciudad).Include(p => p.Empresa).Include(p => p.TipoPagaduria);
+            return View(pagaduria.ToList());
+        }
+
+        // GET: Pagadurias/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Pagaduria pagaduria = db.Pagaduria.Find(id);
+            if (pagaduria == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pagaduria);
+        }
+
+        // GET: Pagadurias/Create
+        public ActionResult Create()
+        {
+            ViewBag.ciu_codigo = new SelectList(db.Ciudad, "ciu_codigo", "ciu_nombre");
+            ViewBag.emp_nit = new SelectList(db.Empresa, "emp_nit", "emp_nombre");
+            ViewBag.tpa_id = new SelectList(db.TipoPagaduria, "tpa_id", "tpa_descripcion");
+            return View();
+        }
+
+        // POST: Pagadurias/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "pag_id,pag_codigo,pag_nit,pag_dv,pag_nombre,pag_direccion,ciu_codigo,pag_telefonos,pag_url,pag_mail,pag_logo,pag_urlsygnus,pag_color,tpa_id,emp_nit,ent_id,pag_activa,pag_fax")] Pagaduria pagaduria)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Pagaduria.Add(pagaduria);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ciu_codigo = new SelectList(db.Ciudad, "ciu_codigo", "ciu_nombre", pagaduria.ciu_codigo);
+            ViewBag.emp_nit = new SelectList(db.Empresa, "emp_nit", "emp_nombre", pagaduria.emp_nit);
+            ViewBag.tpa_id = new SelectList(db.TipoPagaduria, "tpa_id", "tpa_descripcion", pagaduria.tpa_id);
+            return View(pagaduria);
+        }
+
+        // GET: Pagadurias/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Pagaduria pagaduria = db.Pagaduria.Find(id);
+            if (pagaduria == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ciu_codigo = new SelectList(db.Ciudad, "ciu_codigo", "ciu_nombre", pagaduria.ciu_codigo);
+            ViewBag.emp_nit = new SelectList(db.Empresa, "emp_nit", "emp_nombre", pagaduria.emp_nit);
+            ViewBag.tpa_id = new SelectList(db.TipoPagaduria, "tpa_id", "tpa_descripcion", pagaduria.tpa_id);
+            return View(pagaduria);
+        }
+
+        // POST: Pagadurias/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "pag_id,pag_codigo,pag_nit,pag_dv,pag_nombre,pag_direccion,ciu_codigo,pag_telefonos,pag_url,pag_mail,pag_logo,pag_urlsygnus,pag_color,tpa_id,emp_nit,ent_id,pag_activa,pag_fax")] Pagaduria pagaduria)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(pagaduria).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ciu_codigo = new SelectList(db.Ciudad, "ciu_codigo", "ciu_nombre", pagaduria.ciu_codigo);
+            ViewBag.emp_nit = new SelectList(db.Empresa, "emp_nit", "emp_nombre", pagaduria.emp_nit);
+            ViewBag.tpa_id = new SelectList(db.TipoPagaduria, "tpa_id", "tpa_descripcion", pagaduria.tpa_id);
+            return View(pagaduria);
+        }
+
+        // GET: Pagadurias/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Pagaduria pagaduria = db.Pagaduria.Find(id);
+            if (pagaduria == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pagaduria);
+        }
+
+        // POST: Pagadurias/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Pagaduria pagaduria = db.Pagaduria.Find(id);
+            db.Pagaduria.Remove(pagaduria);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
