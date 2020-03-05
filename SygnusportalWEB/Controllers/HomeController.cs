12a81857afbf824d1fa599f5070adc7bf29ac774
malomaan <malomaan@msn.com>
2020-03-05 14:46:48 -0500

    Actualización de hojas de Style

--- SygnusportalWEB/Controllers/HomeController.cs
+++ SygnusportalWEB/Controllers/HomeController.cs
@@ -3,14 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SygnusportalBD;
 
 namespace SygnusportalWEB.Controllers
 {
     public class HomeController : Controller
     {
+        private sygnusportalEntities db = new sygnusportalEntities();
+
         public ActionResult Index()
         {
-            return View();
+            var TPagaduria = db.Pagaduria_ListActivasPeriodos().ToList();
+            return View(TPagaduria);
+
         }
 
         public ActionResult About()
